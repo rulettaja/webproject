72e04ac1f8dd8a100b0299e7060e63afc001c84d
EliasKko <eliaskko@metropolia.fi>
2026-04-15 14:55:18 +0300

    Uudistuksia vitun saatana

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const gigRoutes = require("./routes/gigRoutes");
 
+app.use(cors()); // 👈 ADD THIS
 app.use(express.json());
 app.use("/", gigRoutes);
 
