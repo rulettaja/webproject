8ca10adcfa57fe80eae0ed0038d91dad53deb1bb
rulaaja <roope.rajala@metropolia.fi>
2026-04-18 10:30:19 +0300

    Update db.js connection config

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Apinamies88!",
+  password: "",
   database: "band_gigs"
 });
 
